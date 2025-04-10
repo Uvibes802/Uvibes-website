cf9ba00522b5071a988faf9c50c622041770c846
JustineC31 <justine-cousin@hotmail.com>
2025-04-10 23:06:41 +0200

    style: fix style floatingMenu

--- src/styles/menu/styledFloatingMenu.tsx
+++ src/styles/menu/styledFloatingMenu.tsx
@@ -1,16 +1,16 @@
 import { Fab } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
-export const StyledFloatButton = styled(Fab)(
-	`
-        position: fixed;
-    bottom: 0;
-    right: 0;
-    margin: 16px;
-        z-index: 1000;
-        dropshadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
-        background-color: #ffffff;
-        z-index: 1000;
-     
-    `,
-);
+export const StyledFloatButton = styled(Fab)(({ theme }) => ({
+	position: "fixed",
+	bottom: 0,
+	right: 0,
+	margin: 16,
+	zIndex: 1000,
+	backgroundColor: "#ffffff",
+	dropshadow: "0px 4px 20px rgba(0, 0, 0, 0.2)",
+
+	[theme.breakpoints.up(768)]: {
+		display: "none",
+	},
+}));
