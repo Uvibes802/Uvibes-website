d8ca635569b78bb14d2e9ff5f2f0a940b9bff8af
JustineC31 <justine-cousin@hotmail.com>
2025-04-10 16:53:00 +0200

    feat: creat styledModal and styledFloatingMenu

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
+"use client";
+
+import FloatingMenu from "@/components/FloatingMenu";
+
 export default function Home() {
-  return (
-    <div>
-      <h1>Hello World</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
-      </p>
-    </div>
-  );
+	return (
+		<div>
+			<h1>Hello World</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quos.
+			</p>
+			<FloatingMenu />
+		</div>
+	);
 }
---
+++ src/styles/menu/styledFloatingMenu.tsx
@@ -0,0 +1,12 @@
+import { Fab } from "@mui/material";
+import { styled } from "@mui/material/styles";
+
+export const StyledFloatButton = styled(Fab)(
+	`
+        position: fixed;
+        z-index: 1000;
+        dropshadow: 0px 4px 20px rgba(0, 0, 0, 0.2);
+        background-color: #ffffff;
+     
+    `,
+);
