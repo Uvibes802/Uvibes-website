def208b787341313a08023dfbdda1e2f398cf642
JustineC31 <justine-cousin@hotmail.com>
2025-04-12 19:17:11 +0200

    fix : after pull

--- src/components/cards/teamCards.tsx
+++ src/components/cards/teamCards.tsx
@@ -1,16 +1,16 @@
 import Image from "next/image";
 import "../../styles/cards/teamCard.css";
-import { TeamProps } from "@/types/team/teamProps";
+import type { TeamProps } from "@/types/team/teamProps";
 
 export default function TeamCards({ image, name, role }: TeamProps) {
-  return (
-    <article className="team-card">
-      <div />
-      <Image src={image} alt="team-staff" width={500} height={500} />
-      <div className="content-card">
-        <p>{name}</p>
-        <p>{role}</p>
-      </div>
-    </article>
-  );
+	return (
+		<article className="team-card">
+			<div />
+			<Image src={image} alt="team-staff" width={500} height={500} />
+			<div className="content-card">
+				<p>{name}</p>
+				<p>{role}</p>
+			</div>
+		</article>
+	);
 }
