a09676f742afca8979de82dbc2280c587c42b9c7
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-16 20:21:18 +0200

    fix style

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,6 @@ import { HeroBanner } from "../components/banner/heroBanner";
 export default function Home() {
 	return (
 		<div>
-			<HeroBanner
-				subtitle=" "
-				title="Ouvrir notre esprit à des visions nouvelles"
-				description="La première application qui transforme notre perception de l&apos;autre et nous inspire"
-				image={MockupHome}
-				alt="visuel de l'application"
-			/>
 
 			<FloatingMenu />
 		</div>
