cc51547d0f4e42c49c8f7e0232af3f613b629524
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-17 22:38:42 +0200

    feat: create style for beneficeshomesecton

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 "use client";
-
+import mockupHome from "../../public/images/mockupHome.png"
+import { HeroBanner } from "@/components/banner/heroBanner";
 import FloatingMenu from "@/components/menu/Menu";
+import { BeneficesHomeSection } from "@/components/section/BeneficesHomeSection";
 
 export default function Home() {
   return (
-    <div>
+    <>
+    <HeroBanner subtitle="" title="Ouvrir notre esprit à des visions nouvelles" description="La première application qui transforme notre perception de l’autre et nous inspire" image={mockupHome} alt="visuel application"/>
+      <BeneficesHomeSection />
       <FloatingMenu />
-    </div>
+    </>
   );
 }
--- src/components/section/beneficesHomeSection.tsx
+++ src/components/section/beneficesHomeSection.tsx
@@ -1,10 +1,31 @@
-export function beneficesHomeSection() {
+import { beneficesData } from "./beneficesData";
+import "../../styles/section/BeneficesHomeSection.css";
+import { BeneficesHomeItem } from "./beneficesHomeItem";
+import Button from "../button/Button";
+
+export function BeneficesHomeSection() {
   return (
-    <div>
-        <h2>Transformez votre quotidien</h2>
-        <div>
-            
+    <>
+      <div className="benefices-home-section-container">
+        <h2 className="benefices-home-title">
+          <span id="benefices-home-title-word">Transformez</span> votre
+          quotidien
+        </h2>
+        <div className="benefices-home-data-container">
+          <div className="benefices-home-data">
+            {beneficesData.map((item) => (
+              <BeneficesHomeItem key={item.id} {...item} />
+            ))}
+          </div>
         </div>
-    </div>
+      </div>
+      <div className="benefices-home-button-container">
+        <Button
+          title="Découvrir les bénéfices"
+          type="button"
+          className="benefices-home-button"
+        />
+      </div>
+    </>
   );
 }
