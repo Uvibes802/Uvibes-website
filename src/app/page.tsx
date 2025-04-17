02322e16ad5c09737be2016fa72a096c77526e13
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-17 22:53:15 +0200

    fix: for push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import mockupHome from "../../public/images/mockupHome.png"
+import mockupHome from "../../public/images/mockupHome.png";
 import { HeroBanner } from "@/components/banner/heroBanner";
 import FloatingMenu from "@/components/menu/Menu";
 import { BeneficesHomeSection } from "@/components/section/BeneficesHomeSection";
@@ -7,7 +7,13 @@ import { BeneficesHomeSection } from "@/components/section/BeneficesHomeSection"
 export default function Home() {
   return (
     <>
-    <HeroBanner subtitle="" title="Ouvrir notre esprit à des visions nouvelles" description="La première application qui transforme notre perception de l’autre et nous inspire" image={mockupHome} alt="visuel application"/>
+      <HeroBanner
+        subtitle=""
+        title="Ouvrir notre esprit à des visions nouvelles"
+        description="La première application qui transforme notre perception de l'autre et nous inspire"
+        image={mockupHome}
+        alt="visuel application"
+      />
       <BeneficesHomeSection />
       <FloatingMenu />
     </>
