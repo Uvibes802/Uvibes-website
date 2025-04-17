173dd31d6f329ff457b0afbdb7be262442c88b63
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-17 23:03:53 +0200

    fix: for push

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,8 @@ export default function Home() {
       <HeroBanner
         subtitle=""
         title="Ouvrir notre esprit à des visions nouvelles"
-        description="La première application qui transforme notre perception de l'autre et nous inspire"
+        //prettier-ignore
+        description="La première application qui transforme notre perception de l&apos;autre et nous inspire"
         image={mockupHome}
         alt="visuel application"
       />
