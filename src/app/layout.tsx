d6a1b8ba8461fd887b6a901fdc294dd1cb86b192
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-17 23:08:32 +0200

    fix: for push &apos;

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,22 +2,23 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-	title: "Uvibes",
-	description:
-		"Uvibes est une application web dédiée aux échanges vidéo autour de thématiques variées. Elle permet aux utilisateurs de partager leurs expériences et leurs connaissances à travers des discussions enrichissantes. Grâce à Uvibes, apprenez des autres et contribuez à une communauté où le savoir se transmet par l'interaction.",
-	icons: {
-		icon: "/images/iconUvibes.svg",
-	},
+  title: "Uvibes",
+  description:
+    //prettier-ignore
+    "Uvibes est une application web dédiée aux échanges vidéo autour de thématiques variées. Elle permet aux utilisateurs de partager leurs expériences et leurs connaissances à travers des discussions enrichissantes. Grâce à Uvibes, apprenez des autres et contribuez à une communauté où le savoir se transmet par l&apos;interaction.",
+  icons: {
+    icon: "/images/iconUvibes.svg",
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: Readonly<{
-	children: React.ReactNode;
+  children: React.ReactNode;
 }>) {
-	return (
-		<html lang="fr">
-			<body>{children}</body>
-		</html>
-	);
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
 }
