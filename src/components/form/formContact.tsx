92a9acd2ef2a4188962788f1ddcfb8006c92b621
Nino Jautee <jauteenino@gmail.com>
2025-04-17 20:04:01 +0200

    feat: add logic for contactForm

--- src/components/form/formContact.tsx
+++ src/components/form/formContact.tsx
@@ -2,28 +2,60 @@ import Button from "../button/Button";
 import Input from "../input/Input";
 
 import "../../styles/form/formContact.css";
+import { useForm, type SubmitHandler } from "react-hook-form";
+import type { FormData } from "@/types/form/form";
+
 export default function FormContact() {
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<FormData>();
+
+  const onSubmitHandler: SubmitHandler<FormData> = (data) => {
+    console.log("Données du formulaire:", data);
+    reset();
+  };
+
   return (
-    <form className="form-contact">
+    <form className="form-contact" onSubmit={handleSubmit(onSubmitHandler)}>
       <fieldset>
-        <Input label="Nom" type="text" placeholder="Nom" htmlFor="lastname" />
+        <Input
+          label="Nom"
+          type="text"
+          placeholder="Nom"
+          htmlFor="lastname"
+          {...register("lastname", { required: true })}
+        />
         <Input
           label="Prénom"
           type="text"
           placeholder="Prénom"
           htmlFor="firstname"
+          {...register("firstname", { required: true })}
         />
       </fieldset>
-      <Input label="Email" type="email" placeholder="Email" htmlFor="email" />
+      <Input
+        label="Email"
+        htmlFor="email"
+        type="email"
+        placeholder="Email"
+        {...register("email", { required: true })}
+      />
       <Input
         label="Message"
-        type="text"
-        placeholder="Message"
+        type="textarea"
+        {...register("message", { required: true })}
         htmlFor="message"
       />
       <div>
         <Button title="Envoyer" type="submit" />
       </div>
+      {errors.lastname && <p>Nom est requis</p>}
+      {errors.firstname && <p>Prénom est requis</p>}
+      {errors.email && <p>Email est requis</p>}
+      {errors.message && <p>Message est requis</p>}
     </form>
   );
 }
