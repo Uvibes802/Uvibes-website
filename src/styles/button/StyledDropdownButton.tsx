f3838da0ef6b4fcad84084a7799b37494dca8515
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-22 19:54:14 +0200

    feat: add zindex to styledBottomNav

--- src/styles/button/StyledDropdownButton.tsx
+++ src/styles/button/StyledDropdownButton.tsx
@@ -1,42 +1,42 @@
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import {
-	Accordion,
-	AccordionDetails,
-	AccordionSummary,
-	styled,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  styled,
 } from "@mui/material";
 
 export const StyledDropdownButton = styled(Accordion)(({ theme }) => ({
-	background: "white",
-	color: "var(--mainColor)",
-	cursor: "pointer",
-	borderRadius: "calc(var(--border-radius)*2) !important",
-	boxShadow: "1px 3px 10px 0px #0000006b",
-	width: "80%",
-	position: "relative",
-	border: "1px solid var(--mainColor)",
-	overflow: "hidden",
-	transition: "all 0.3s ease",
+  background: "white",
+  color: "var(--mainColor)",
+  cursor: "pointer",
+  borderRadius: "calc(var(--border-radius)*2) !important",
+  boxShadow: "1px 3px 10px 0px #0000006b",
+  width: "80%",
+  position: "relative",
+  border: "1px solid var(--mainColor)",
+  overflow: "hidden",
+  transition: "all 0.3s ease",
 
-	"&:hover": {
-		opacity: 0.9,
-	},
+  "&:hover": {
+    opacity: 0.9,
+  },
 
-	"&.Mui-expanded": {
-		background: "var(--mainColor)",
-		color: "white",
-		[theme.breakpoints.up(768)]: {
-			margin: "0 !important",
-			width: "100% !important",
-			order: 1,
-		},
-	},
+  "&.Mui-expanded": {
+    background: "var(--mainColor)",
+    color: "white",
+    [theme.breakpoints.up(768)]: {
+      margin: "0 !important",
+      width: "100% !important",
+      order: 1,
+    },
+  },
 
-	[theme.breakpoints.up(768)]: {
-		flex: "1 ",
-		width: "auto",
-		margin: 0,
-	},
+  [theme.breakpoints.up(768)]: {
+    flex: "1 ",
+    width: "auto",
+    margin: 0,
+  },
 }));
 
 export const StyledAccordionSummary = styled(AccordionSummary)`
@@ -76,49 +76,47 @@ export const StyledAccordionDetails = styled(AccordionDetails)`
   background: white;
   color: var(--mainColor);
   font-family: calc(var(--font-size-h2) / 2.5);
-  font-weight: bold ;
+  font-weight: bold;
   text-align: center;
-
- 
 `;
 
 export const StyledDropdownButtonContainer = styled("div")(({ theme }) => ({
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
-	margin: "var(--spacing-ref) 0",
-	width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  margin: "var(--spacing-ref) 0",
+  width: "100%",
 
-	[theme.breakpoints.up(768)]: {
-		margin: "var(--spacing-ref)",
-		width: "calc(33.33% - var(--spacing-ref) * 2)",
-		maxWidth: "400px",
-		alignSelf: "flex-start",
-	},
+  [theme.breakpoints.up(768)]: {
+    margin: "var(--spacing-ref)",
+    width: "calc(33.33% - var(--spacing-ref) * 2)",
+    maxWidth: "400px",
+    alignSelf: "flex-start",
+  },
 
-	"&:has(.Mui-expanded)": {
-		[theme.breakpoints.up(768)]: {
-			width: "100%",
-			maxWidth: "100%",
-			order: 1,
-		},
-	},
+  "&:has(.Mui-expanded)": {
+    [theme.breakpoints.up(768)]: {
+      width: "100%",
+      maxWidth: "100%",
+      order: 1,
+    },
+  },
 }));
 
 export const DropdownButtonGroup = styled("div")(({ theme }) => ({
-	display: "flex",
-	flexDirection: "column",
-	alignItems: "center",
-	width: "100%",
-	padding: "0 calc(var(--spacing-ref) * 2)",
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  width: "100%",
+  padding: "0 calc(var(--spacing-ref) * 2)",
 
-	[theme.breakpoints.up(768)]: {
-		display: "flex",
-		flexDirection: "row",
-		flexWrap: "wrap",
-		justifyContent: "center",
-		alignItems: "flex-start",
-		gap: "var(--spacing-ref) * 2",
-		padding: "0 calc(var(--spacing-ref) * 2)",
-	},
+  [theme.breakpoints.up(768)]: {
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+    alignItems: "flex-start",
+    gap: "var(--spacing-ref) * 2",
+    padding: "0 calc(var(--spacing-ref) * 2)",
+  },
 }));
--- src/styles/menu/StyledBottomNavigation.tsx
+++ src/styles/menu/StyledBottomNavigation.tsx
@@ -2,21 +2,22 @@ import { BottomNavigation } from "@mui/material";
 import { styled } from "@mui/material/styles";
 
 export const StyledBottomNavigation = styled(BottomNavigation)(({ theme }) => ({
-	position: "fixed",
-	bottom: 0,
-	left: "50%",
-	transform: "translateX(-50%)",
-	display: "flex",
-	justifyContent: "center",
-	alignItems: "center",
-	gap: "2rem",
-	backgroundColor: theme.palette.background.paper,
-	boxShadow: theme.shadows[4],
-	marginBottom: "var(--spacing-ref)",
-	borderRadius: "calc(var(--border-radius)*2.5)",
-	padding: "var(--spacing-ref)",
+  position: "fixed",
+  zIndex: 9999,
+  bottom: 0,
+  left: "50%",
+  transform: "translateX(-50%)",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "2rem",
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[4],
+  marginBottom: "var(--spacing-ref)",
+  borderRadius: "calc(var(--border-radius)*2.5)",
+  padding: "var(--spacing-ref)",
 
-	[theme.breakpoints.down(768)]: {
-		display: "none",
-	},
+  [theme.breakpoints.down(768)]: {
+    display: "none",
+  },
 }));
