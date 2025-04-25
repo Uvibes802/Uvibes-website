e00f96aa6f8896881ea80702cb81844dd8f2742e
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-25 23:47:32 +0200

    fix: refacto functSection style

--- src/components/section/functSection.tsx
+++ src/components/section/functSection.tsx
@@ -1,32 +1,33 @@
 import FunctCards from "../cards/functCards";
-import iconCall from "../../../public/images/icon _Video Person Call_.png";
-import iconUser from "../../../public/images/icon _Users.png";
-import iconBook from "../../../public/images/material-symbols_menu-book-outline.png";
 import "../../styles/section/functSection.css";
+import { BookOpenText, HeartHandshake, MessagesSquare } from "lucide-react";
 export default function FunctSection() {
   return (
     <section className="funct-section">
-      <h2>Les Fonctionnalités</h2>
+      <h2 className="title-h2">Les Fonctionnalités</h2>
       <div className="funct-container">
         <FunctCards
-          image={iconCall}
+          icone={
+            <HeartHandshake size="100%" style={{ color: "var(--mainColor)" }} />
+          }
           title={"Echanges"}
           subtitle={
             "Partager des témoignages positifs sur un nombre infini de sujets"
           }
-          link={"/functionnalities"}
         />
         <FunctCards
-          image={iconUser}
+          icone={
+            <MessagesSquare size="100%" style={{ color: "var(--mainColor)" }} />
+          }
           title={"Participation"}
           subtitle={"Recueillir régulièrement les avis de son collectif"}
-          link={"/functionnalities"}
         />
         <FunctCards
-          image={iconBook}
+          icone={
+            <BookOpenText size="100%" style={{ color: "var(--mainColor)" }} />
+          }
           title={"Ressources"}
           subtitle={"Disposer d'une cité des savoirs sur-mesure"}
-          link={"/functionnalities"}
         />
       </div>
     </section>
