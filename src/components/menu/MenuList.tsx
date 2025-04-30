88711d6298793445382b38e6b67ffd116d2eb747
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-04-30 11:54:43 +0200

    fix: style

--- src/components/menu/MenuList.tsx
+++ src/components/menu/MenuList.tsx
@@ -4,16 +4,22 @@ import { Items } from "../../features/menu/MenuData";
 import MenuItem from "./MenuItem";
 import { PopupButton } from "react-calendly";
 import "../../styles/menu/Menu.css";
+import { useEffect, useState } from "react";
 
 type MenuListProps = {
   className: string;
 };
 
 export default function MenuList({ className }: MenuListProps) {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
   return (
     <>
       {Items.map((item) =>
-        item.id === 6 ? (
+        item.id === 6 && isClient ? (
           <PopupButton
             key={item.id}
             url="https://calendly.com/justine-cousin-wbx/demo-uvibes"
