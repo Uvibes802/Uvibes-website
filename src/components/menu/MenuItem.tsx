e97a72d24b5c8a7ef6eab59518bf473616a5796e
Justine-Cousin <justine.cousin.wbx@gmail.com>
2025-05-19 22:04:42 +0200

    fix: effect to navbar

--- src/components/menu/MenuItem.tsx
+++ src/components/menu/MenuItem.tsx
@@ -1,10 +1,23 @@
 import "../../styles/menu/MenuItem.css";
 import type { MenuItemProps } from "@/types/menu/MenuItemProps";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function MenuItems({ icon, label, color, link }: MenuItemProps) {
+  const pathname = usePathname();
+  const isActive = pathname === link;
+
   return (
-    <Link href={link} passHref className="items-link">
+    <Link
+      href={link}
+      passHref
+      className="items-link"
+      style={{
+        fontWeight: isActive ? 800 : 400,
+        transform: isActive ? "scale(1.2)" : "scale(1)",
+        transition: "all 0.2s ease",
+      }}
+    >
       {icon}
       <p className="items-label" style={{ color }}>
         {label}
